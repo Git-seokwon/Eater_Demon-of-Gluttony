bf1c2d78b8df544eb93d6ae4bec0a5467476901b
unxunsx <secho1018@gmail.com>
2024-08-27 15:51:13 +0900

    effect02

--- Project Eater/Assets/Scripts/Core/Entity/StateMachine/State/Player/SkillState/ChargingSkillState.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-// CastingSkillState¿Í µ¿ÀÏ
-public class ChargingSkillState : PlayerSkillState
-{
-}
--- Project Eater/Assets/Scripts/Environment/Environment.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[DisallowMultipleComponent]
-public class Environment : MonoBehaviour
-{
-    public SpriteRenderer sprite;
-}
