a86e674ecbf804c7c377461b0ad58badc22f6f86
Git-seokwon <suck758@naver.com>
2025-01-24 06:14:26 +0900

    오류수정이랑상호작용
    상호작용 테스트를 위한 오브젝트는 싹다 비활성화 해놓고 쓰세요

--- Project Eater/Assets/Scripts/Core/UI/PlayerInteraction.cs
+++ Project Eater/Assets/Scripts/Core/UI/PlayerInteraction.cs
@@ -1,22 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 
 // 상호작용할 오브젝트에 붙여놓을 Monobehaviour 스크립트 
 public class PlayerInteraction : MonoBehaviour
 {
     [SerializeField] private GameObject target; // 상호작용할 Target.
+    [SerializeField] private GameObject interactionField; // 옆에 띄울 UI 캔버스
+    [SerializeField] private Dictionary<string, string> actions; // 상호작용 목록
 
-    [SerializeField] private string name;
-    [SerializeField] private string action;
+    private PlayerInteractionUI pui;
+
+    private void Awake()
+    {
+        GameObject obj = Instantiate(interactionField);
+        RectTransform objscale = obj.GetComponent<RectTransform>();
+        obj.transform.SetParent(transform, false);
+        float pos = (gameObject.transform.localScale.x) / 2 + (objscale.rect.width) / 2;
+        obj.transform.localPosition = new Vector3(pos, objscale.rect.height/2, 0);
+        obj.SetActive(true);
+
+        pui = obj.GetComponent<PlayerInteractionUI>();
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.Equals(target))
         {
-            //Debug.Log("TriggerEnter");
-            PlayerInteractionUI pui = collision.gameObject.GetComponentInChildren<PlayerInteractionUI>(true);
-            pui.AddAction(name, action);
+            //pui.AddAction(codeName, actions);
         }
     }
 
@@ -24,9 +36,7 @@ public class PlayerInteraction : MonoBehaviour
     {
         if (collision.gameObject.Equals(target))
         {
-            //Debug.Log("TriggerExit");
-            PlayerInteractionUI pui = collision.gameObject.GetComponentInChildren<PlayerInteractionUI>(true);
-            pui.DeleteAction(name);
+            //pui.DeleteAction(codeName);
         }
     }
 }
